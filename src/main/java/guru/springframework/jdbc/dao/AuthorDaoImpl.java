adbab2ec02659b5fac1f27490c2a584165912298
Mihai Ocneanu <mihai.ocneanu@gmail.com>
2023-02-10 18:51:51 +0200

    impl save new author

--- src/main/java/guru/springframework/jdbc/dao/AuthorDaoImpl.java
+++ src/main/java/guru/springframework/jdbc/dao/AuthorDaoImpl.java
@@ -28,7 +28,14 @@ public class AuthorDaoImpl implements AuthorDao {
 
     @Override
     public Author saveNewAuthor(Author author) {
-        return null;
+        jdbcTemplate.update(
+                "INSERT INTO author (first_name, last_name) VALUES (?,?)",
+                author.getFirstName(), author.getLastName()
+        );
+
+        Long insertedId = jdbcTemplate.queryForObject("SELECT LAST_INSERT_ID()", Long.class);
+
+        return getById(insertedId);
     }
 
     @Override
